2ccbcb22ef5ccf8166c43d768ec9f5153d03c455
XelHaku <tamez.jm@gmail.com>
2014-12-29 16:36:24 -0600

    fix listview RecordLog
    use of listview('refresh')

--- record_log_display2.js
+++ record_log_display2.js
@@ -7,7 +7,7 @@ $(function(){
     
 		  
 	  wRecArray = JSON.parse(window.localStorage.getItem("ProgressLog"));
-	var newUlist = document.createElement("ul");
+	
 	var name = [];
 	var newlistelement = [];
 	var newtable = [];
@@ -22,18 +22,93 @@ $(function(){
 		name[i] = "item" + String(i);
 		$(newlistelement[i]).id = name[i] ;
 		$(newlistelement[i]).text(""+name[i]);
+
+		name[i] = null;
+		newlistelement[i] = document.createElement( "li" );
 		
-			
+		newlink[i] = document.createElement("a");
+	 	
+		newtable[i] = document.createElement("table");
+		newtable[i].style.width= "100%";
+		newtable[i].border=0;
+		newtable[i].cellspacing=2;
+		newtable[i].cellpadding=0;
+		
+/////////////////////////////
+	//calculate IMC
+    var imctext = 10000*parseFloat(wRecArray[i].weight)/(parseFloat(wRecArray[i].height)*parseFloat(wRecArray[i].height));
+	imctext=imctext.toFixed(2);
+	
+	$(newtable[i]).append('<tr>');
+	
+	//compare and set color for WEIGHT 
+	if(i==0){
+		
+		
+	$(newtable[i]).append('<th width="25%" scope="col" align="center"><span>'+  wRecArray[i].weight +'</span></th>');
+	
+	}
+	else{
+		var compare = wRecArray[i-1].weight-wRecArray[i].weight;
+		if(compare!=0){
+				if (compare >= 0)	
+				{
+	$(newtable[i]).append('<th width="25%" scope="col" align="center"><span style="color:#ADDA55">'+  wRecArray[i].weight +'</span></th>');
+						}else{
+	$(newtable[i]).append('<th width="25%" scope="col" align="center"><span style="color:#E51616">'+  wRecArray[i].weight +'</span></th>');				
+				}
+		}else{
+        $(newtable)[i].append('<th width="25%" scope="col" align="center"><span>'+  wRecArray[i].weight +'</span></th>');
+		}
+		
+	}
+	
+
+	//compare and set color for BODYFAT
+	if(i==0){
+		
+		
+	$(newtable[i]).append('<th width="25%" scope="col" align="center"><span>'+  wRecArray[i].bodyfat +'</span></th>');
+	
+	}
+	else{
+		var compare = wRecArray[i-1].bodyfat-wRecArray[i].bodyfat;
+		if(compare!=0){
+				if (compare >= 0)	
+				{
+	$(newtable[i]).append('<th width="25%" scope="col" align="center"><span style="color:#ADDA55">'+  wRecArray[i].bodyfat +'</span></th>');
+						}else{
+	$(newtable[i]).append('<th width="25%" scope="col" align="center"><span style="color:#E51616">'+  wRecArray[i].bodyfat +'</span></th>');				
+				}
+		}else{
+        $(newtable[i]).append('<th width="25%" scope="col" align="center"><span>'+  wRecArray[i].bodyfat +'</span></th>');
+		}
+		
+	}
+
+		
+	//PRINT IMC
+	$(newtable[i]).append('<th width="25%" scope="col" align="center">'+  imctext +'</th>');
+		 
+	$(newtable[i]).append('<th width="25%" scope="col" align="center">'+ wRecArray[i].day+"/"+wRecArray[i].month+"/"+wRecArray[i].year +'     </th>');
+	
+	$(newtable[i]).append('</tr>');  
+
+
+
+
+
+
+/////////////////////////////			
 	
 	
 //append everything
-       
+    $(newlink[i]).append(newtable[i]);
     $(newlistelement[i]).append(newlink[i]);
 
-	$(newUlist).append(newlistelement[i]);  
+	$('#recordloglist').append(newlistelement[i]);  
+	$('#recordloglist').listview('refresh');
 	
-	$("#bottomTitle").append(newUlist);  //alert("end"+i);
-    
 
 } //end for 
 
